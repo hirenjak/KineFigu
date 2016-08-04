3415bf64cf9bc62d32ad86a6ba3d339d5ca56609
hirenjak <hirenjak@gmail.com>
2016-08-05 04:17:04 +0900

    別のリファクタリング＆今度こそ連携成功してると信じて

--- KineFigu/Vector2PLUS.cs
+++ KineFigu/Vector2PLUS.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 
 namespace KineFigu
 {
@@ -14,18 +8,23 @@ namespace KineFigu
         public float X;
         public float Y;
 
+        // コンストラクタ
         public Vector2PLUS(float X, float Y)
         {
             this.X = X;
             this.Y = Y;
         }
 
+        // キャスト関連
         public static implicit operator Vector2(Vector2PLUS target) { return new Vector2(target.X, target.Y); }
 
+        // 四則演算
         public static Vector2PLUS operator +(Vector2PLUS target, Vector2PLUS other) { return new Vector2PLUS(target.X + other.X, target.Y + other.Y); }
         public static Vector2PLUS operator -(Vector2PLUS target, Vector2PLUS other) { return new Vector2PLUS(target.X - other.X, target.Y - other.Y); }
 
         public static Vector2PLUS operator *(Vector2PLUS target, float other) { return new Vector2PLUS(target.X * other, target.Y * other); }
         public static Vector2PLUS operator /(Vector2PLUS target, float other) { if (other == 0) { return new Vector2PLUS(0, 0); } return new Vector2PLUS(target.X / other, target.Y / other); }
+
+        // 内積・外積
     }
 }
