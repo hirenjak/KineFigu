d6a306198ce131b23406a14428ac1a2e37f69535
Negi <s1426092DR@s.chibakoudai.jp>
2016-08-05 21:46:42 +0900

    tete

--- TestLeapMotion/Program.cs
+++ TestLeapMotion/Program.cs
@@ -17,6 +17,8 @@ namespace TestLeapMotion
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            ///
         }
     }
 }
