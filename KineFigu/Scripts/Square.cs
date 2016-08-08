52720746918e121cc763f363fcda00fbf12ea59f
hirenjak <hirenjak@gmail.com>
2016-08-08 12:05:22 +0900

    多分、本日最終版

--- KineFigu/Scripts/Square.cs
+++ KineFigu/Scripts/Square.cs
@@ -8,12 +8,12 @@ namespace KineFigu
     {
         public Square(Vector2PLUS initPosi, Vector2PLUS size) : base(initPosi, size)
         {
-            texture = Content.Load<Texture2D>("Square");
+            texture = Content.Load<Texture2D>("Square2");
         }
 
         public Square(Vector2PLUS initPosi, Vector2PLUS size, bool gravityFlag, bool collisionFlag) : base(initPosi, size, gravityFlag, collisionFlag)
         {
-            texture = Content.Load<Texture2D>("Square");
+            texture = Content.Load<Texture2D>("Square2");
         }
     }
 }
